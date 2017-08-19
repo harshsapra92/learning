e1dffb593cdc96c851379391a4eed74d6a2773f4
harshsapra92 <harsh@Harshs-MacBook-Pro.local>
2017-08-19 23:25:54 +0530

    Finished webpack tutorial

---
+++ webpack-tutorial/bear.js
@@ -0,0 +1,5 @@
+var $ = require('jquery');
+
+require('style-loader!css-loader!./bear.css');
+
+module.exports = $('<div/>').html('Growl!');
---
+++ webpack-tutorial/index.js
@@ -0,0 +1,3 @@
+var bearHtml = require('./bear.js');
+
+document.body.appendChild(bearHtml[0]);
